6bb2ae0618cf6b92081a00a2f1c0e79358d7c90a
Gustavo Fernandes <gustavofernandba@gmail.com>
2022-10-03 17:37:28 -0300

    Adiciona arquivo Service com requisição da API Git

--- quests/challenge-github-app/GitHubApp/Service/Service.swift
+++ quests/challenge-github-app/GitHubApp/Service/Service.swift
@@ -7,10 +7,71 @@
 
 import Foundation
 
+enum ServiceError: Error {
+    case badUrl
+    case taskError
+    case responseError
+    case dataError
+    case parsedError
+    case invalidStatusCode(Int)
+    
+    var localizedDescription: String {
+        switch self {
+            
+        case .badUrl:
+            return "url invalida"
+        case .taskError:
+            return "task invalida"
+        case .responseError:
+            return "response invalido"
+        case .dataError:
+            return "data invalida"
+        case .parsedError:
+            return "parsed invalido"
+        case .invalidStatusCode(let statusCode):
+            return "status code invalido \(statusCode)"
+        }
+    }
+}
+
 struct Service {
-
-    func fetchList(_ completion: ([String]) -> Void) {
-
-        completion(["Repository 1", "Repository 2", "Repository 3"])
+    
+    let urlSession: URLSession
+    
+    init(urlSession: URLSession = .shared){
+        self.urlSession = urlSession
+    }
+    
+    func fetchList(user: String, onComplete: @escaping (Result<[Repository], ServiceError>) -> Void) {
+        
+        guard let url = URL(string: "https://api.github.com/users/\(user)/repos") else {
+            return onComplete(.failure(.badUrl))
+        }
+        
+        let task = urlSession.dataTask(with: url) { data, response, error in
+            if error != nil {
+                return onComplete(.failure(.taskError))
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                return onComplete(.failure(.responseError))
+            }
+            
+            if !(200...299 ~= response.statusCode) {
+                return onComplete(.failure(.invalidStatusCode(response.statusCode)))
+            }
+            
+            guard let data = data else {
+                return onComplete(.failure(.dataError))
+            }
+            
+            do {
+                let result = try JSONDecoder().decode([Repository].self, from: data)
+                onComplete(.success(result))
+            } catch {
+                return onComplete(.failure(.parsedError))
+            }
+        }
+        task.resume()
     }
 }
